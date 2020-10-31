81581371ae47993639122cf0855ebcc0d83d654b
Rahul <rahulmbme@gmail.com>
2020-10-31 16:50:38 +0530

    Code review
    Added error mapping to webpage so if someone eneter the wrong url they redirect to an error page which has a link to send them back to home page. Added comments in controller class so that once you add more requestmappings it will be easier to determine which one is for what.
    P.S for some reason the maps on home page weren't showing up on my browser, i tried fixing it but couldn't, you might wanna test it on a couple different browsers, i tried on chrome.

--- src/main/java/com/restaurantpicker/RestaurantpickerController.java
+++ src/main/java/com/restaurantpicker/RestaurantpickerController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RestaurantpickerController 
 {
+
+	// request mapping for home page
 	@RequestMapping("/")
 	public String start()
 	{
 		return "index";
 	}
 	
+	//request mapping for restaurant choice page with different restaurants and ratings
 	@RequestMapping("/choices")
 	public String index()
 	{
