26ec92972ad7ac00a4855198fa18c6003b792ea5
Rahul <rahulmbme@gmail.com>
2020-11-22 20:00:37 +0530

    Rahul Bhagwat Code Review 2
    Formatted code in accordance with javascript and java best practices as stated by google and GitHub for compatibility with most browsers.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -2,63 +2,63 @@
 function initMap() {
 
     // The location of UC
-    const uc = { lat: 39.130146, lng: -84.518982 };
+    const UC = { lat: 39.130146, lng: -84.518982 };
     //Mac Shack
-    const mac = { lat: 39.128246, lng: -84.517241 };
+    const MAC = { lat: 39.128246, lng: -84.517241 };
     //HangOverEasy
-    const hangOverEasy = { lat: 39.130280, lng: -84.510176 };
+    const HANG_OVER_EASY = { lat: 39.130280, lng: -84.510176 };
     
     
-    // The location of Kroger
-    const kroger = { lat: 39.106677, lng: -84.513777 };
-    //The Eagle 39.110695, -84.515235
-    const eagle = { lat: 39.110695, lng: -84.515235 };
-    //Goodfellas 39.109180, -84.511776
-    const goodfellas = { lat: 39.109180, lng: -84.511776 };
+    // The location of KROGER
+    const KROGER = { lat: 39.106677, lng: -84.513777 };
+    //The EAGLE 39.110695, -84.515235
+    const EAGLE = { lat: 39.110695, lng: -84.515235 };
+    //GOODFELLAS 39.109180, -84.511776
+    const GOODFELLAS = { lat: 39.109180, lng: -84.511776 };
     
     
     // The map, centered at UC
-    const mapUC = new google.maps.Map(document.getElementById("mapUC"), {
+    const MAP_UC = new google.maps.Map(document.getElementById("MAP_UC"), {
       zoom: 15,
       center: uc,
     });
     
-      const mapCincy = new google.maps.Map(document.getElementById("mapCincy"), {
+      const MAP_CINCY = new google.maps.Map(document.getElementById("MAP_CINCY"), {
         zoom: 15,
-        center: kroger,
+        center: KROGER,
       
     });
     
     // The marker, positioned at UC
     const markerUC = new google.maps.Marker({
         position: uc,
-        map: mapUC,
+        map: MAP_UC,
       });
     // The marker, positioned at Mac Shack
       const markerMac = new google.maps.Marker({
         position: mac,
-        map: mapUC,
+        map: MAP_UC,
       });
     // The marker, positioned at HangOverEasy
       const markerHangOverEasy = new google.maps.Marker({
-        position: hangOverEasy,
-        map: mapUC,
+        position: HANG_OVER_EASY,
+        map: MAP_UC,
       });
     
-      // The marker, positioned at kroger
-    const markerKroger = new google.maps.Marker({
-        position: kroger,
-        map: mapCincy,
+      // The marker, positioned at KROGER
+    const markerKROGER = new google.maps.Marker({
+        position: KROGER,
+        map: MAP_CINCY,
       });
-    // The marker, positioned at The Eagle
-      const markerEagle = new google.maps.Marker({
-        position: eagle,
-        map: mapCincy,
+    // The marker, positioned at The EAGLE
+      const markerEAGLE = new google.maps.Marker({
+        position: EAGLE,
+        map: MAP_CINCY,
       });
-    // The marker, positioned at Goodfellas
-      const markerGoodfellas = new google.maps.Marker({
-        position: goodfellas,
-        map: mapCincy,
+    // The marker, positioned at GOODFELLAS
+      const markerGOODFELLAS = new google.maps.Marker({
+        position: GOODFELLAS,
+        map: MAP_CINCY,
       });
 
     }
