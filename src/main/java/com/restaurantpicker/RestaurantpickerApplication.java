4d8785281f9682064ccba5d6475f6f90d6131fda
Administrator <Administrator@DESKTOP-099UQRG>
2020-11-22 17:25:26 -0500

    Created Trentaml-CodeReview-2 Branch. Changed the names of the Constants in the Map function in the .js files. Removed unused imports from the Application.java. Auto-formatted the .html files for html best practices.
    The project compiled and ran, however, I was unable to get the
    index.html page to load. I was able to view the restaurantchoice.html
    page though. Chcek your controller to make sure it is correctly pulling
    your index.html page when the project is run.

--- src/main/java/com/restaurantpicker/RestaurantpickerApplication.java
+++ src/main/java/com/restaurantpicker/RestaurantpickerApplication.java
@@ -1,7 +1,6 @@
 package com.restaurantpicker;
 
-import java.util.Arrays;
-import java.util.List;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/resources/static/restaurantchoice.js
+++ src/main/resources/static/restaurantchoice.js
@@ -17,63 +17,63 @@
 function initMap() {
 
     // The location of UC
-    const uc = { lat: 39.130146, lng: -84.518982 };
+    const UC = { lat: 39.130146, lng: -84.518982 };
     //Mac Shack
-    const mac = { lat: 39.128246, lng: -84.517241 };
+    const MAC = { lat: 39.128246, lng: -84.517241 };
     //HangOverEasy
-    const hangOverEasy = { lat: 39.130280, lng: -84.510176 };
+    const HANG_OVER_EASY = { lat: 39.130280, lng: -84.510176 };
     
     
     // The location of Kroger
-    const kroger = { lat: 39.106677, lng: -84.513777 };
+    const KROGER = { lat: 39.106677, lng: -84.513777 };
     //The Eagle 39.110695, -84.515235
-    const eagle = { lat: 39.110695, lng: -84.515235 };
+    const EAGLE = { lat: 39.110695, lng: -84.515235 };
     //Goodfellas 39.109180, -84.511776
-    const goodfellas = { lat: 39.109180, lng: -84.511776 };
+    const GOODFELLAS = { lat: 39.109180, lng: -84.511776 };
     
     
     // The map, centered at UC
-    const mapUC = new google.maps.Map(document.getElementById("mapUC"), {
+    const MAP_UC = new google.maps.Map(document.getElementById("mapUC"), {
       zoom: 15,
-      center: uc,
+      center: UC,
     });
     
-      const mapCincy = new google.maps.Map(document.getElementById("mapCincy"), {
+      const MAP_CINCY = new google.maps.Map(document.getElementById("mapCincy"), {
         zoom: 15,
-        center: kroger,
+        center: KROGER,
       
     });
     
     // The marker, positioned at UC
-    const markerUC = new google.maps.Marker({
-        position: uc,
-        map: mapUC,
+    const MARKER_UC = new google.maps.Marker({
+        position: UC,
+        map: MAP_UC,
       });
     // The marker, positioned at Mac Shack
-      const markerMac = new google.maps.Marker({
-        position: mac,
-        map: mapUC,
+      const MARKER_MAC = new google.maps.Marker({
+        position: MAC,
+        map: MAP_UC,
       });
     // The marker, positioned at HangOverEasy
-      const markerHangOverEasy = new google.maps.Marker({
-        position: hangOverEasy,
-        map: mapUC,
+      const MARKER_HANG_OVER_EASY = new google.maps.Marker({
+        position: HANG_OVER_EASY,
+        map: MAP_UC,
       });
     
       // The marker, positioned at kroger
-    const markerKroger = new google.maps.Marker({
-        position: kroger,
-        map: mapCincy,
+    const MARKER_KROGER = new google.maps.Marker({
+        position: KROGER,
+        map: MAP_CINCY,
       });
     // The marker, positioned at The Eagle
-      const markerEagle = new google.maps.Marker({
-        position: eagle,
-        map: mapCincy,
+      const MARKER_EAGLE = new google.maps.Marker({
+        position: EAGLE,
+        map: MAP_CINCY,
       });
     // The marker, positioned at Goodfellas
-      const markerGoodfellas = new google.maps.Marker({
-        position: goodfellas,
-        map: mapCincy,
+      const MARKER_GOODFELLAS = new google.maps.Marker({
+        position: GOODFELLAS,
+        map: MAP_CINCY,
       });
 
     }
